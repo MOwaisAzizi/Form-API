1e72b029330a38a0436b1609a078db14b8eb1bbb
owias azizi <owaisazizi@jmail.com>
2024-11-30 18:24:07 +0430

    update

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -3,27 +3,25 @@ import axios from 'axios'
 import './FullPost.css'
 
 class FullPost extends React.Component {
-  state={
-    LoadedPost:null
+  state = {
+    LoadedPost: null
   }
-  componentDidUpdate(){
-    axios.get(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`).then((response)=>{
-      this.setState({LoadedPost : response.data})
+  componentDidUpdate() {
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`).then((response) => {
+      this.setState({ LoadedPost: response.data })
     })
   }
- deleteHandler=()=>{
-  axios.delete(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`).then((response)=>{
-    console.log(response);
-    
-  })
+  deleteHandler = () => {
+    axios.delete(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`).then((response) => {
+    })
   }
 
   render() {
     let post = <p style={{ textAlign: 'center' }}>Please select a Post</p>
-    if(this.props.id){
+    if (this.props.id) {
       <p>please Wait</p>
     }
-   if(this.state.LoadedPost){
+    if (this.state.LoadedPost) {
       post = (
         <div className="full-post">
           <h2>{this.state.LoadedPost.title}</h2>
@@ -34,7 +32,6 @@ class FullPost extends React.Component {
             </button>
           </div>
         </div>
-   
       )
     }
     return post
--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 import './NewPost.css'
 
 class NewPost extends React.Component {
-
   state = {
     title: '',
     content: '',
